19b66370112d2300e59109758ab5f3e8aced1ad5
JiYounWi <117698364+JiYounWi@users.noreply.github.com>
2023-10-01 20:54:51 +0900

    Update KakaoTokenManagement.js

--- components/KakaoTokenManagement.js
+++ components/KakaoTokenManagement.js
@@ -1,46 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import {View, Text, Button, StyleSheet, TouchableOpacity, Alert} from 'react-native';
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-//다른방법 참고 시 https://www.notion.so/64769b0e2a0c4519ab8a3fcf1a0319dc?pvs=4 링크 참고
 function KakaoTokenManagement ({navigation}) {
     const [accessToken, setAccessToken] = useState(null);
 
     useEffect(() => {
-    // AsyncStorage에서 데이터를 가져오는 로직
-    const fetchAccessToken = async () => {
+        const fetchAccessToken = async () => {
         try {
+            //저장된 토큰 값 들고오기
             const storedData = await AsyncStorage.getItem('accessToken');
+
+            //만약 토큰 값이 null이 아니라면
             if (storedData !== null) {
-              setAccessToken(storedData); // JSON.parse()를 사용하지 않음
+            setAccessToken(storedData);
+    
+            // API 호출
+            axios.post('https://todohaemil.com/users/validate', null, {
+                headers: {
+                    Authorization: `Bearer ${storedData}`,
+                },
+                })
+                .then((response) => {
+                console.log('AT 검증 결과', response.data);
+                })
+                .catch((error) => {
+                console.error('AT 검증 에러 : ', error);
+                });
             }
         } catch (error) {
             console.error('Error fetching data:', error);
         }
-    };
-
-    fetchAccessToken();
-  }, []);
-
-    // const accessToken = AsyncStorage.getItem('accessToken');
-
-  console.log('엑세스토큰 : ',accessToken);
-
-    axios.get('https://todohaemil.com/users/validate',  {
-        headers : {
-            Authorization:`Bearer ${accessToken}`}
-            // headers : {
-            //     Authorization: `Bearer 
-            //     ${accessToken}`,
-            //     "cccept": "application/json",}
-    })
-    .then((response)=>{
-        console.log('엑세스 토큰 결과' , response.data);
-    })
-    .catch((error)=>{
-        console.error('엑세스 토큰 get 에러 : ',error);
-    })
+        };
+    
+        fetchAccessToken();
+    }, []);
 }
 
 export default KakaoTokenManagement;
